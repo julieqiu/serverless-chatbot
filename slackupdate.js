518e258450c0bab8fda18b48baa5530ec780135e
Julie Qiu <julieyeqiu@gmail.com>
2017-10-09 23:47:16 -0400

    [5.7] Add getShortUrl to slackupdate.js

--- slackupdate.js
+++ slackupdate.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const request = require('request');
 const aws = require('aws-sdk');
 const https = require('https');
 const qs = require('querystring');
@@ -12,6 +13,33 @@ const getSignedUrl = function(bucket, key) {
         resolve(url);
     });
 };
+
+const getShortUrl = function(url) {
+  console.log('LONG URL', url);
+  console.log(process.env.SHORTENER_API_URL + qs.stringify({key: process.env.SHORTENER_API_KEY}));
+
+  return new Promise((resolve, reject) => {
+      var req = {
+          uri: process.env.SHORTENER_API_URL + qs.stringify({key: process.env.SHORTENER_API_KEY}),
+          method: 'POST',
+          json: true,
+          body: {
+              longUrl: url
+          }
+      }
+
+      request(req, (err, res, body) => {
+          console.log(res.statusCode);
+          console.log(err);
+          if (err && res.statusCode !== 200) {
+              console.log(err);
+              reject(err);
+          } else {
+              resolve(body.id);
+          }
+      });
+  });
+}
 
 const writeToSlack = function(url) {
   return new Promise((resolve, reject) => {
@@ -34,7 +62,9 @@ module.exports.execute = (event, context, callback) => {
   const bucket = event.Records[0].s3.bucket.name;
   const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
 
+  console.log('executing');
   getSignedUrl(bucket, key)
+    .then((url) => getShortUrl(url))
     .then((url) => writeToSlack(url))
     .then(() => callback(null))
     .catch((err) => callback(err))
