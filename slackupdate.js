63406c5b18b2a678e3fdeefdab41b76934874935
Julie Qiu <julieyeqiu@gmail.com>
2017-10-09 23:43:48 -0400

    [5.1] Update slackupdate.js to create signedURL in Slack

--- slackupdate.js
+++ slackupdate.js
@@ -1,7 +1,41 @@
 'use strict';
 
-module.exports.execute = (event, context, callback) => {
-  console.log(event);
+const aws = require('aws-sdk');
+const https = require('https');
+const qs = require('querystring');
+const s3 = new aws.S3();
 
-  callback(null);
+const getSignedUrl = function(bucket, key) {
+    return new Promise((resolve, reject) => {
+        const params = {Bucket: bucket, Key: key, Expires: 604800};
+        var url = s3.getSignedUrl('getObject', params);
+        resolve(url);
+    });
+};
+
+const writeToSlack = function(url) {
+  return new Promise((resolve, reject) => {
+    const response = {
+        token: process.env.BOT_ACCESS_TOKEN,
+        channel: process.env.CHANNEL_ID,
+        text: url
+      }
+
+      const slackurl = process.env.POST_MESSAGE_URL + qs.stringify(response);
+
+      https.get(slackurl, (res) => {
+        const statusCode = res.statusCode;
+        resolve();
+      })
+  });
+}
+
+module.exports.execute = (event, context, callback) => {
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+
+  getSignedUrl(bucket, key)
+    .then((url) => writeToSlack(url))
+    .then(() => callback(null))
+    .catch((err) => callback(err))
 };
