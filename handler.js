b9036f42e5b29c3e7c7be92239b9fd727ddaa88b
Julie Qiu <julieyeqiu@gmail.com>
2017-10-09 23:29:26 -0400

    [2.9] Update handler to return back messsage sent by bot

--- handler.js
+++ handler.js
@@ -1,16 +1,43 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const https = require('https');
+const qs = require('querystring');
 
-  callback(null, response);
+const direct_mention = new RegExp('^\<\@' + process.env.BOT_ID + '\>', 'i');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+
+module.exports.endpoint = (event, context, callback) => {
+  const request = JSON.parse(event.body);
+
+  if (request.type === 'url_verification' &&
+    request.token === process.env.VERIFICATION_TOKEN) {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        'challenge': request.challenge
+      })
+    }
+
+    return callback(null, response);
+  }
+
+  if (request.event.text.match(direct_mention)) {
+    const response = {
+      token:process.env.BOT_ACCESS_TOKEN,
+      channel:request.event.channel,
+      text:request.event.text.replace(direct_mention, '') + ' [' + Date.now() + ']'
+    }
+
+    const URL = process.env.POST_MESSAGE_URL + qs.stringify(response);
+
+    https.get(URL, (res) => {
+      const statusCode = res.statusCode;
+
+      if (statusCode !== 200) {
+        console.log(res);
+      }
+
+      callback(null, {statusCode:200});
+    })
+  }
 };
